04c527ba472e97a5f8fc5ae3845122c2c1b904cb
istarlet <istarlet@gmail.com>
2022-06-12 22:48:01 -0400

    Add more styles

---
+++ main.js
@@ -0,0 +1,4 @@
+const proress = document.getElementById("progress-steps")
+const previous = document.getElementById("previous")
+const next = document.getElementById("next")
+const circle = document.querySelectorAll(".circle")
